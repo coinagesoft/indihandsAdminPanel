ba5266ed0be407ebce8393fa7a8f0ea050cd1cda
Pradnya129 <pradnyamandekar129@gmail.com>
2026-02-18 23:38:59 +0530

    fix sparticuz launch final1

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: [
+      "@sparticuz/chromium",
+      "puppeteer-core"
+    ],
+  },
+};
+
+export default nextConfig;
