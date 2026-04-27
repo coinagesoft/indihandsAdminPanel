d5c61a98c23eb4531c0bb1e654a0467c7886cba8
Pradnya129 <pradnyamandekar129@gmail.com>
2026-04-27 10:30:35 +0530

    config update

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ✅ REQUIRED for GitHub Pages
-
-  basePath: '/NaukriAdminPanel',
-  assetPrefix: '/NaukriAdminPanel/',
-
-  images: {
-    unoptimized: true, // ✅ required for static export
-  },
-
   experimental: {
     serverComponentsExternalPackages: [
       "@sparticuz/chromium",
